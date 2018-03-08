3b875ebe1ec9c416011ca9e9cf85eaec70e8d55c
TakingTheL <yliu6@stuy.edu>
2018-03-08 13:14:50 -0500

    classwork

--- object.js
+++ object.js
@@ -1,9 +1,46 @@
 var pic = document.getElementById("vimage");
 var clearButton = document.getElementById("clear");
+const initDotRad = 30;
 
 const clear = function(){
     while (pic.firstChild){
 	pic.removeChild(pic.firstChild);
+    }
+}
+
+var makeDot = function(x, y){
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c.setAttribute("cx", x);
+    c.setAttribute("cy", y);
+    c.setAttribute("r", initDotRad);
+    c.setAttribute("fill", "red");
+    c.setAttribute("stroke", "black");
+
+    return c;
+}
+
+var dot = {
+    var x;
+    var y;
+    var radius;
+    var color;
+    var display = function(){
+	drawDot(
+    }
+    var getX = function(){
+	return x;
+    }
+    var getY = function(){
+	return y;
+    }
+    var setX = function(newX){
+	x = newX;
+    }
+    var setY = function(newY){
+	y = newY;
+    }
+    var getColor = function(){
+	return color;
     }
 }
 
